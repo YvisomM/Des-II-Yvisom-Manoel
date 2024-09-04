047c7f92a66eec1a6d2470ebeb455598388a0a2c
Yvisom Manoel <yvisom2012@gmail.com>
2024-09-04 20:33:41 -0300

    Aula de Python

---
+++ Python/Calculadora.py
@@ -0,0 +1,14 @@
+a = int(input("Digite um numero: "))
+
+b = int(input("Digite outro numero: "))
+
+print('{} + {} = '.format(a,b))
+print(a + b)
+print('{} - {} = '.format(a,b))
+print(a - b)
+print('{} * {} = '.format(a,b))
+print(a * b)
+print('{} / {} = '.format(a,b))
+print (a / b)
+print('{} % {} = '.format(a,b))
+print (a % b)
---
+++ Python/Hipotenusa.py
@@ -0,0 +1,6 @@
+a = int(4)
+b = int(3)
+
+hipotenusa = (a * a + b * b)
+              
+print(hipotenusa)
